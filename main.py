a572c22d33eb68240fb36001c9387a4e6055d626
sandman-code <caden.crist.2019@gmail.com>
2022-02-06 16:48:27 -0500

    fixed error

--- main.py
+++ main.py
@@ -5,30 +5,11 @@ from comm.lx16a import *
 if __name__ == '__main__':
     JT = calc_jacobian(0, 0, (np.pi / 2))
     torque = JT @ np.array([0,0,10])
-    alpha = 0
+    alpha = 95
     beta = 5
     gamma = 95 
 
     
-    LX16A.initialize("COM3")
-    try:
- 
-        servo_16 = LX16A(16)
-        #servo_17 = LX16A(17)
-        #servo_18 = LX16A(18)
-
-    except ServoTimeout as e:
-        print(f"Servo {e.ID} is not responding. Exiting...")
-        exit()
-    
-
-    
-
- 
-        
-    servo_16.moveTimeWrite(alpha, 1000)
-    #servo_17.moveTimeWrite(beta, 1000)
-    #servo_18.moveTimeWrite(gamma, 1000)
 
 
     
