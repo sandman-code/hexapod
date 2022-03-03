f2c2733eb76f9edb63532aa525376262b69a7ea7
sandman-code <caden.crist.2019@gmail.com>
2022-03-03 11:37:14 -0500

    fixed walk

--- motion/controller.py
+++ motion/controller.py
@@ -1,5 +1,9 @@
 from ast import walk
+<<<<<<< HEAD
 from time import time
+=======
+from time import sleep
+>>>>>>> 0f4390042342a70e8f929819cb493d1f6b32f270
 from motion.maths import *
 from math import cos, gamma, sin
 import numpy as np
@@ -184,6 +188,7 @@ def walk(hexapod, v):
             qd_t[l2, t] = np.matmul(np.inverse(J), np.transpose(angles))
     
     for t in range(5):
+<<<<<<< HEAD
         for a in range(6):
             curr_leg = hexapod.legs[a]
             coeffs = trajectory_planning(q_t[a,t], q_t[a, t+1], qd_t[a,t], qd_t[a,t], 0, dt)
@@ -194,6 +199,90 @@ def walk(hexapod, v):
             curr_leg.move_beta(beta, dt)
             curr_leg.move_gamma(gamma, dt)
             time.sleep(1)
+=======
+
+        x_coeff_1 = trajectory_planning(x_pos[0,t], x_pos[0,t+1], x_vel[0,t], x_vel[0,t+1], t, t+1)
+        y_coeff_1 = trajectory_planning(y_pos[0,t], y_pos[0,t+1], y_vel[0,t], y_vel[0,t+1], t, t+1)
+        z_coeff_1 = trajectory_planning(z_pos[0,t], z_pos[0,t+1], z_vel[0,t], z_vel[0,t+1], t, t+1)
+
+        x_coeff_2 = trajectory_planning(x_pos[1,t], x_pos[1,t+1], x_vel[1,t], x_vel[1,t+1], t, t+1)
+        y_coeff_2 = trajectory_planning(y_pos[1,t], y_pos[1,t+1], y_vel[1,t], y_vel[1,t+1], t, t+1)
+        z_coeff_2 = trajectory_planning(z_pos[1,t], z_pos[1,t+1], z_vel[1,t], z_vel[1,t+1], t, t+1)        
+
+        x_coeff_3 = trajectory_planning(x_pos[2,t], x_pos[2,t+1], x_vel[2,t], x_vel[2,t+1], t, t+1)
+        y_coeff_3 = trajectory_planning(y_pos[2,t], y_pos[2,t+1], y_vel[2,t], y_vel[2,t+1], t, t+1)
+        z_coeff_3 = trajectory_planning(z_pos[2,t], z_pos[2,t+1], z_vel[2,t], z_vel[2,t+1], t, t+1)
+
+        x_coeff_4 = trajectory_planning(x_pos[3,t], x_pos[3,t+1], x_vel[3,t], x_vel[3,t+1], t, t+1)
+        y_coeff_4 = trajectory_planning(y_pos[3,t], y_pos[3,t+1], y_vel[3,t], y_vel[3,t+1], t, t+1)
+        z_coeff_4 = trajectory_planning(z_pos[3,t], z_pos[3,t+1], z_vel[3,t], z_vel[3,t+1], t, t+1)
+
+        x_coeff_5 = trajectory_planning(x_pos[4,t], x_pos[4,t+1], x_vel[4,t], x_vel[4,t+1], t, t+1)
+        y_coeff_5 = trajectory_planning(y_pos[4,t], y_pos[4,t+1], y_vel[4,t], y_vel[4,t+1], t, t+1)
+        z_coeff_5 = trajectory_planning(z_pos[4,t], z_pos[4,t+1], z_vel[4,t], z_vel[4,t+1], t, t+1)
+
+        x_coeff_6 = trajectory_planning(x_pos[5,t], x_pos[5,t+1], x_vel[5,t], x_vel[5,t+1], t, t+1)
+        y_coeff_6 = trajectory_planning(y_pos[5,t], y_pos[5,t+1], y_vel[5,t], y_vel[5,t+1], t, t+1)
+        z_coeff_6 = trajectory_planning(z_pos[5,t], z_pos[5,t+1], z_vel[5,t], z_vel[5,t+1], t, t+1)
+
+        i = 0
+        dt = 0.5
+
+        while i < 1:
+
+            alpha1 = cubic_traj(x_coeff_1, i)
+            beta1 = cubic_traj(y_coeff_1, i)
+            gamma1 = cubic_traj(z_coeff_1, i)
+
+            alpha2 = cubic_traj(x_coeff_2, i)
+            beta2 = cubic_traj(y_coeff_2, i)
+            gamma2 = cubic_traj(z_coeff_2, i)
+
+            alpha3 = cubic_traj(x_coeff_3, i)
+            beta3 = cubic_traj(y_coeff_3, i)
+            gamma3 = cubic_traj(z_coeff_3, i)
+
+            alpha4 = cubic_traj(x_coeff_4, i)
+            beta4 = cubic_traj(y_coeff_4, i)
+            gamma4 = cubic_traj(z_coeff_4, i)
+
+            alpha5 = cubic_traj(x_coeff_5, i)
+            beta5 = cubic_traj(y_coeff_5, i)
+            gamma5 = cubic_traj(z_coeff_5, i)
+
+            alpha6 = cubic_traj(x_coeff_6, i)
+            beta6 = cubic_traj(y_coeff_6, i)
+            gamma6 = cubic_traj(z_coeff_6, i)
+
+            
+            leg1.move_alpha(alpha1, 6)
+            leg1.move_beta(beta1, 6)
+            leg1.move_gamma(gamma1, 6)
+
+            leg2.move_alpha(alpha2, 6)
+            leg2.move_beta(beta2, 6)
+            leg2.move_gamma(gamma2, 6)
+
+            leg3.move_alpha(alpha3, 6)
+            leg3.move_beta(beta3, 6)
+            leg3.move_gamma(gamma3, 6)
+            
+            leg4.move_alpha(alpha4, 6)
+            leg4.move_beta(beta4, 6)
+            leg4.move_gamma(gamma4, 6)
+
+            leg5.move_alpha(alpha5, 6)
+            leg5.move_beta(beta5, 6)
+            leg5.move_gamma(gamma5, 6)
+
+            leg6.move_alpha(alpha6, 6)
+            leg6.move_beta(beta6, 6)
+            leg6.move_gamma(gamma6, 6)
+
+            sleep(1)
+
+            i += dt
+>>>>>>> 0f4390042342a70e8f929819cb493d1f6b32f270
             
 
     
